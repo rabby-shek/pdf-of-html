33ef09bd78dfebcefbc5ac9076e46979965f7034
suvo-khan <mdsuvoshek1@gmail.com>
2022-12-29 14:23:00 +0600

    added

---
+++ index.js
@@ -0,0 +1,8 @@
+var doc = new jsPDF();
+
+ function saveDiv(divId, title) {
+ doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+ doc.save('div.pdf');
+}
+
+
